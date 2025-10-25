762f137dca1b5e11f84646056fb3cdb432612142
suruthiA16 <suruthiarunachalam16@gmail.com>
2025-10-25 20:14:03 +0530

    Added .gitignore and Looping statements example

---
+++ ForAndForeachExample.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasics
+{
+    public class ForAndForeachExample
+    {
+        public void forForEach()
+        {
+            int[] arrayValues = [101, 102, 103, 104, 105, 106];
+
+            //For example
+            Console.WriteLine("Example of for loop");
+            for (int i = 0; i < arrayValues.Length; i++)
+            {
+                Console.WriteLine(arrayValues[i]);
+            }
+
+            //Foreach example
+            Console.WriteLine("Example of foreach loop");
+            foreach (int i in arrayValues)
+            {
+                Console.WriteLine(i);
+            }
+
+            //continue example with for loop - Print even numbers
+            Console.WriteLine("Enter the target number to get its even numbers");
+            int userTargetInput = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Example of continue");
+            for (int i = 0; i < userTargetInput; i++)
+            {
+                if (i % 2 == 1)
+                    continue; //this will skip odd numbers and continue the loop till the condition met
+                Console.WriteLine(i);
+
+            }
+
+            //break example with for loop - Print till 10 only
+            Console.WriteLine("Example of break");
+            for (int i = 1; i < userTargetInput; i++)
+            {
+                Console.WriteLine(i);
+                if (i == 10)
+                    break; //this will skip the remaining process and will came out of loop               
+            }
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,5 +5,18 @@
 //sW1.userOutput();
 
 //Live example of switch & goto
-CoffeeShopSwitchGoto cSG = new CoffeeShopSwitchGoto();
-cSG.coffeeShop();
+//CoffeeShopSwitchGoto cSG = new CoffeeShopSwitchGoto();
+//cSG.coffeeShop();
+
+//Live example of While loop
+//BurgerShopWhile bSW = new BurgerShopWhile();
+//bSW.burgerShop();
+
+//Basic while & do while example
+//WhileDoWhileExample wDw = new WhileDoWhileExample();
+//wDw.DoWhileBasic();
+
+//Basic for, for each, continue and break examples
+ForAndForeachExample fFE = new ForAndForeachExample();
+fFE.forForEach();
+
